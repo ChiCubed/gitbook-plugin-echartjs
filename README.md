a9982e5b6776b83ed96b53d45aa4ac219423f69e
Albert Smith <albert.chejin.edward.smith@outlook.com>
2017-05-29 18:29:06 +0800

    Finalise

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Extended Chart.js plugin for GitBook
+
+A [Chart.js](http://www.chartjs.org/) wrapper for GitBook. Also see [here](https://github.com/chartjs/gitbook-plugin-chartjs).
+
+However, this plugin is extended: an equation can be given, which is evaluated by the plugin and sent to Chart.js to be displayed.
+
+The syntax is as follows for using normal Chart.js:
+
+```
+{% echartjs width="200" height="100" type="chart" %}
+{
+    ...
+}
+{% endechartjs %}
+```
+
+Put the Chart.js configuration in the object between the two tags. Width and height are obviously modifiable.
+
+To draw an equation, use:
+
+```
+{% echartjs width="200" height="100" type="equation" %}
+{
+    equation: "x^2",
+    start: 0,
+    stop: 100,
+    step: 10,
+    options: {
+        ...
+    }
+}
+{% endechartjs %}
+```
+
+(Note the `type="equation"` in the block.) This will create a chart with type 'line'. Any of the normal Chart.js options can be used within the 'options' object.
+
+Start, stop and step refer to the distance along the x-axis between each point that is drawn by the plugin. You can configure the scales using the 'options', as described [here](http://www.chartjs.org/docs/latest/axes/cartesian/linear.html).
---
+++ index.js
@@ -0,0 +1,80 @@
+var Parser = require('expr-eval').Parser;
+var parser = new Parser();
+
+var countGraph = 0;
+
+function parseConfig(config) {
+  var final = null;
+  
+  try {
+    final = JSON.parse(config);
+  } catch (e) {
+    console.error('[echartjs] Failed to parse chartjs configuration with error', e);
+  }
+  
+  return final || {};
+}
+
+function genHTML(chartID, config, width, height) {
+  let attr = '';
+  attr += ( width != null ?  ' width="' +  width + '"': '');
+  attr += (height != null ? ' height="' + height + '"': '');
+  
+  return '<div class="echartjs-wrapper">' +
+             '<canvas id="' + chartID + '" class="chartjs"' + attr + '>' +
+             '<script>' +
+                 'new Chart(' +
+                     'document.getElementById("' + chartID + '"),' +
+                     JSON.stringify(config) + ');' +
+             '</script>' +
+         '</div>';
+}
+
+
+function processEquation(config) {
+  // Process the configuration file given
+  // that it was passed in as an equation
+  // configuration, and convert it to a valid
+  // chart.js configration.
+  
+  var final = {
+    type: 'line',
+    data: []
+  };
+  final.options = config.options;
+  
+  let equation = config.equation;
+  let start = config.start;
+  let stop = config.stop;
+  let step = config.step;
+  
+  var expr = parser.parse(equation);
+  for (var xpos = start; xpos <= stop; xpos += step) {
+    final.data.push({x: xpos, y: expr.evaluate({x : xpos})});
+  }
+  
+  return final
+}
+
+module.exports = {
+  book: {css: ['style.css']},
+  ebook: {css: ['style.css']},
+  
+  blocks: {
+    echartjs: {
+      process: function(block) {
+        let chartID = 'echartjs-'+countGraph++;
+        var config = readConfig(block.body());
+        let width = block.kwargs.width;
+        let height = block.kwargs.height;
+        
+        if (block.kwargs.type !== undefined &&
+            block.kwargs.type == "equation") {
+          config = processEquation(config);
+        }
+        
+        return genHTML(chartID, config, width, height);
+      }
+    }
+  }
+}
